f39e7f60900b5ff6a3c2a42c3a19fe8bbe841888
CodyLeblanc13 <cody.leblanc@edu.sait.ca>
2026-08-14 09:34:05 -0600

    created managment.py file for creating the functions and base layout of the file.

---
+++ managment.py
@@ -0,0 +1,30 @@
+# Imports the Child class from the Child.py File as a short form "ch"
+import child as ch
+
+def load_children(file_path):
+    pass
+
+def save_children(file_path, child_list):
+    pass
+
+def find_child_index(child_list, child_id):
+    pass
+
+def add_child(child_list):
+    pass
+
+def remove_child(child_list):
+    pass
+
+def check_in_child(child_list):
+    pass
+
+def check_out_child(child_list):
+    pass
+
+def view_all_children(child_list):
+    pass
+
+
+def main():
+    pass
