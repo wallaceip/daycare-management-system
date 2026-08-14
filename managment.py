60b3ddc4cb5e4b85c6ac90e0dc4ae0ea16694637
CodyLeblanc13 <cody.leblanc@edu.sait.ca>
2026-08-14 11:50:20 -0600

    Added comments to the functions

--- managment.py
+++ managment.py
@@ -4,17 +4,30 @@ import child as ch
 def load_children(file_path="child.csv"):
     """Loads data from csv file into a list of child objects"""
 
+    # Initialize empty child list
+    child_list = [] 
 
-    child_list = []
-
+    # opens the csv file 
     with open(file_path) as file:
+
+        # reads lines and strips newline characters
         child = file.read().splitlines()
 
+        # loops through lines in file
         for line in child:
+            
             if line.strip and not line.startswith("child_id"):
+
+                # splits csv into list of strings
                 info = line.split(",")
+
+                # creates the child objects
                 kid = ch.Child(info[0], info[1], info[2], info[3])
+
+                # adds the child objects to the child_list
                 child_list.append(kid)
+
+    # returnes a list of child objects
     return child_list
 
     
@@ -42,6 +55,8 @@ def view_all_children(child_list):
 
 def main_menu():
     """Main menu display user selection validation"""
+
+    # loops until a valid input is selected
     main_menu_dict = {
         1: "Add Child",
         2: "Remove Child",
@@ -51,34 +66,55 @@ def main_menu():
         6: "Exit",
     }
 
-    selection = 0
+    # intial invalid selection to start the loop
+    selection = 0  
 
+    # main body of selection look
     while selection < 1 or selection > 6:
+
+        # formats and prints the menu display
         for key, value, in main_menu_dict.items():
             print(f'{key}) {value}')
 
+        # recieves user input
         user_input = input('Select option: ')
+
+        # validates user input as a digit then converts to integer
         if user_input.isdigit():
             selection = int(user_input)
+
+        #  Displays if user inputs an invalid selection
         if selection < 1 or selection > 6:
             print("invalid option. \n")
+
+    # returns the valid selection as an integer
     return selection
         
 
 # Main function that will run on program start
 def main():
     """Program entry point for initialization and menu handling"""
+
+    # prints main menu welcome banner
     print('\n' + '*'*40)
     print(f"{"Welcome to YYC Daycare System":^40s}")
     print("*"*40)
 
+
+    # prints number children info currently in the system 
     child_list = load_children()
     print(f"{len(child_list)} child record loaded.\n")
 
+    # initial invalid choice to start loop 
     selection = 0 
+
+    # will continue to loop until 6 is input
     while selection != 6:
+
+        # funtion call
         selection = main_menu()
 
+        # match case for user navigation with each case matching to a specific function of the program
         match selection:
             case 1:
                 add_child(child_list)
@@ -90,6 +126,8 @@ def main():
                 check_out_child(child_list)
             case 5:
                 view_all_children(child_list)
+
+                # ends the program and saves the info as a csv 
             case 6:
                 save_children(child_list)
                 print("Data saved. Goodbye. \n")
